9ee5d85219e3f35c5ecb001b28c5be06853ccbf6
bapt2 <5711@holbertonstudents.com>
2022-09-30 14:04:04 -0700

    c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,12 +6,12 @@
  * Return: Exit status code
  */
 
-int main(void);
-({
+int main(void)
+{
 printf("Size of char: %d byte\n", sizeof(char));
 printf("Size of int: %d bytes\n", sizeof(int));
 printf("Size of long int: %d bytes\n", sizeof(long int));
 printf("Size of long long int: %d bytes\n", sizeof(long long int));
 printf("Size of float: %d bytes\n", sizeof(float));
 return (0);
- })
+ }
