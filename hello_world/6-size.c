0c84d4cf27cc2b5ebcc96553955c7c07690ec928
bapt2 <5711@holbertonstudents.com>
2022-09-30 14:02:46 -0700

    c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 
 int main(void);
-{
+({
 printf("Size of char: %d byte\n", sizeof(char));
 printf("Size of int: %d bytes\n", sizeof(int));
 printf("Size of long int: %d bytes\n", sizeof(long int));
 printf("Size of long long int: %d bytes\n", sizeof(long long int));
 printf("Size of float: %d bytes\n", sizeof(float));
 return (0);
-}
+ })
