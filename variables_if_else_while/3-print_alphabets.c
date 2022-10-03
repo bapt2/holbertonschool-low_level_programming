0159387445553f6ad1924e68732fdf8cb2dee90f
bapt2 <5711@holbertonstudents.com>
2022-10-03 08:01:11 -0700

    c if else

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main- Main function
+ *
+ *Return: Exit status code
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'A' ; alphabet <= 'Z' ; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
