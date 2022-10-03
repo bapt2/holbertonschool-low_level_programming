45e63d1290fe25f3cec89cc11e1b3f9460e88837
bapt2 <5711@holbertonstudents.com>
2022-10-03 03:19:33 -0700

    c if else

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("is positive");
+printf("95 is positive ");
 }
 else if (n == 0)
 {
-printf("is zero");
+printf("0 is zero ");
 }
 else if (n < 0)
 {
-printf("is negative");
+printf("-95 is negative ");
 }
 return (0);
 }
