db501bceecbb817981290615e4160272847844e1
bapt2 <5711@holbertonstudents.com>
2022-10-03 13:41:24 -0700

    c if else

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main- Main function
+ *
+ *Return: Exit status code
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
