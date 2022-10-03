1c627c2e6455bfc64f9c2e54a240b65c4b63ac28
bapt2 <5711@holbertonstudents.com>
2022-10-03 13:25:14 -0700

    c if else

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main- Main function
+ *
+ *Return: Exit status code
+ */
+int main(void)
+{
+int x = 0;
+while (x < 10)
+{
+putchar(x + '0');
+if (x < 9)
+{
+putchar(',');
+putchar(' ');
+}
+x++;
+}
+putchar('\n');
+return (0);
+}
