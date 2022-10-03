dd09c81a65524e75a4ba555c7e56b3f38cab8ed2
bapt2 <5711@holbertonstudents.com>
2022-10-03 12:49:37 -0700

    c if else

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main- Main function
+ *
+ *Return: Exit status code
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'z' ; alphabet <= 'a' ; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
