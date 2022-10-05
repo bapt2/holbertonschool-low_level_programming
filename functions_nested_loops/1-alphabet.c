76b1f10cb8e438a2c22fd59cabde4716daa2bb94
bapt2 <5711@holbertonstudents.com>
2022-10-05 02:19:41 -0700

    c function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - check the code
+ *
+ *Return: always 0
+ */
+print_alphabet(void)
+{
+char alph;
+for (alph = 'a' ; alph <= 'z' ; alph++)
+{
+putchar(alph);
+}
+putchar('\n');
+return (0);
+}
