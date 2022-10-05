eb8eda44b03438147b28160909b0ba32891e4175
bapt2 <5711@holbertonstudents.com>
2022-10-05 10:50:18 -0700

    c function

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
- *print_alphabet - Retry point
+ *print_alphabet_x10 - Retry point
  *
  *Return: always 0
  */
 void print_alphabet_x10(void)
 {
+int x = 0;
 char c;
+for (x = 0; x < 10; x++)
+{
 for (c = 'a'; c <= 'z'; c++)
-for (c = 0; c < 10; c++)
 {
 _putchar(c);
 }
 _putchar('\n');
+}
 }
