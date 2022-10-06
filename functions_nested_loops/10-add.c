eb8fb894f2d7a881914571facef5685e6574138c
bapt2 <5711@holbertonstudents.com>
2022-10-06 04:16:21 -0700

    c function

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - function that add two integer
+ *@z: number being added
+ *@y: number being added
+ *Return: the result of the addition
+ */
+int add(int z, int y)
+{
+return (z + y);
+}
