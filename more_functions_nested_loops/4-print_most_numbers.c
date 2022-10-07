f36d00ee14f090c1b51a100bb64b361ce02eb44e
bapt2 <5711@holbertonstudents.com>
2022-10-07 03:52:29 -0700

    c more funcions

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_most_numbers - function that print the numbers from 0 to 9 without 2 and 4
+ *
+ *Return: always 0
+ */
+void print_most_numbers(void)
+{
+char num;
+for (num = '0' ; num <= '9' ; num++)
+{
+if (num != '2' && num != '4')
+putchar(num);
+}
+putchar('\n')
+}
