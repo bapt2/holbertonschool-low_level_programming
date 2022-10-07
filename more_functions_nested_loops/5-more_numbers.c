fc87dcc3935edcc4a09a006af1ee13c3bf1f0891
bapt2 <5711@holbertonstudents.com>
2022-10-07 06:27:43 -0700

    c more funcions

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,17 @@
  */
 void more_numbers(void)
 {
-int x;
 int n;
+int x;
 for (x = 0; x < 10; x++)
 {
 for (n = 0 ; n < 14 ; n++)
 {
 if (n > 9)
 {
-_putchar((x / 10) + '0');
+_putchar((n / 10) + '0');
 }
-_putchar((x % 10) + '0');
+_putchar((n % 10) + '0');
 }
 _putchar('\n');
 }
