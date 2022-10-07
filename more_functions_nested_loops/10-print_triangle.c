da20f31d25e59284c0581b29fc29162e0ed998ff
bapt2 <5711@holbertonstudents.com>
2022-10-07 15:00:48 -0700

    c more functions

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - prints a square
+ * @size: size of the square
+ * Return: Alaways 0 (Success)
+ */
+void print_triangle(int size)
+{
+int i = 0, j;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+while (i < size)
+{
+j = 0;
+while (j < size)
+{
+_putchar('#');
+j++;
+}
+if (j <= size - 1)
+_putchar(' ');
+_putchar('\n');
+i++;
+}
+}
