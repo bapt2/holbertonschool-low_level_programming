8c7964fe44e06990cfcdbe434011f7af78f041a2
bapt2 <5711@holbertonstudents.com>
2022-10-07 07:15:35 -0700

    c more funcions

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_line - function that draws a straight line
+ *
+ *Return: always 0
+ */
+void print_line(int n)
+{
+while (n > 0)
+{
+_putchar('_');
+--n;
+}
+_putchar('\n');
+}
