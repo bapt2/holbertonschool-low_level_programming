f11b237bad5e950aeeb0cf6ea88e239827833acf
bapt2 <5711@holbertonstudents.com>
2022-10-07 04:34:37 -0700

    c more funcions

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *more numbers - function that print numbers 10 times the numbers
+ *
+ *Return: always 0
+ */
+void more_numbers(void)
+{
+int x = 0;
+char num;
+for (x = 0; x < 10; x++)
+{
+for (num = '0' ; num <= "14" ; num++)
+{
+_putchar(num);
+}
+_putchar('\n');
+}
+}
