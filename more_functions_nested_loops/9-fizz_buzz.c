cd0c6237eadcfda2afa9f51802e71a6d52832790
bapt2 <5711@holbertonstudents.com>
2022-10-07 14:51:43 -0700

    c more functions

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints numbers from 0 to 14 ten times
+ * Return: Alaways 0 (Success)
+ */
+
+int main(void)
+{
+int x;
+for (x = 1; x <= 100 ; num++)
+{
+if (x % 3 == 0 && x % 5 == 0)
+printf("FizzBuzz");
+else if (x % 5 == 0)
+printf("Buzz");
+else if (x % 3 == 0)
+printf("Fizz");
+else
+printf("%d", x);
+if (x !=100)
+printf(" ");
+}
+return (0 * putchar(10));
+}
