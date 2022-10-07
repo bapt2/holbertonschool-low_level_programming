c597d901d530700aa8092e500435bcbdae2e3730
bapt2 <5711@holbertonstudents.com>
2022-10-07 02:19:12 -0700

    c more functions

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.c"
+/**
+ *mul - function that multiplies 2 integer
+ *@a: parameter of the function
+ *@b: parameter of the function
+ *Return:
+ */
+int mul(int a, int b);
+{
+return(a * b);
+}
