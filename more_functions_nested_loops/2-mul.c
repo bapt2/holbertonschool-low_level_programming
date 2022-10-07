fc229759dfb11b2ac305f30d45134338dee4d577
bapt2 <5711@holbertonstudents.com>
2022-10-07 02:43:57 -0700

    c more functions

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  *mul - function that multiplies 2 integer
- *@a: parameter of the function
- *@b: parameter of the function
+ *@a: number being multiple
+ *@b: number being multiple
  *Return:
  */
 int mul(int a, int b)
 {
-return(a * b);
+return (a * b);
 }
