b7715a1bdaf692a7b8a39f2bf35c0025a160a336
bapt2 <5711@holbertonstudents.com>
2022-10-07 03:18:29 -0700

    c more functions

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - function that print the numbers, from 0 to 9
+ *
+ *Return: always 0
+ */
+void print_numbers(void)
+{
+char num;
+for (num = '0' ; num < '9' ; num++)
+{
+_putchar(num);
+}
+_putchar('\n')
+return (0);
+}
