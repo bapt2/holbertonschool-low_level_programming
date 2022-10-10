76c98cb2d9adb49934c8722f295892b4a30af2f8
bapt2 <5711@holbertonstudents.com>
2022-10-10 03:13:12 -0700

    c pointers

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,10 @@
 #include "main.h"
 /**
  *reset_to_98 - function that takes a pointer to an int as parameter
- *
+ *@n: parameter of the function
  *Return always 0
  */
-int main(void)
+void reset_to_98(int *n)
 {
-  int n = 98;
-  int *n = 402;
-  swapnum(&a, &b);
-  _putchar("n is %d and *n is %d\n", n, *n);
-  return (0);
+*n = 98;
 }
