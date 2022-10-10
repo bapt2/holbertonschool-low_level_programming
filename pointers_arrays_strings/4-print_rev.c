c84680dadc5b289ee97f30d4c7278f5a42263bf9
bapt2 <5711@holbertonstudents.com>
2022-10-10 10:39:04 -0700

    c pointers

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  *print_rev - function that prints a string in reverse
  *@s: character
@@ -8,7 +9,7 @@
 void print_rev(char *s)
 {
 int c = 0;
-while (s[c] !='\0')
+while ('\0' != *s)
 c--;
- _putchar('\n');
+_putchar('\n');
 }
