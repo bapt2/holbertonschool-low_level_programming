39ee318034b9da9936b96dfb716b788a75312319
bapt2 <5711@holbertonstudents.com>
2022-10-12 02:22:27 -0700

    c pointers

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,9 @@ void print_array(int *a, int n)
 	int i = 0;
 	while (i < n)
 	{
-		if (i == 0)
-			printf("%d", a[i]);
-		else
-			printf(", %d", a[i]);
+		printf("%d", a[i]);
+		if (i < (n - 1))
+			printf(", ");
 		i++;
 	}
 	_putchar('\n');
