9832554c3e3f54040d58481a9682840405da5df0
bapt2 <5711@holbertonstudents.com>
2022-10-13 03:02:41 -0700

    c more pointers

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,11 +4,12 @@
  *@dest: second chain
  *@src: first chain
  *@n: integer
- *Return:
+ *Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+
 	while (i < n && src[i] != '\0')
 	{
 		i++;
