cf49d23a4b8d17214bd2a43d35c0e2344bbffcfb
bapt2 <5711@holbertonstudents.com>
2022-10-13 07:38:06 -0700

    c more pointers

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *string_toupper - function that changes all lowercase letter of a string
+ *@s: character
+ *Return: 0
+ */
+char *string_toupper(char *s)
+{
+	int i;
+	for (i = 0; s[i] != '\0';i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}
