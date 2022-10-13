f6c7422552c449a14eaac8f84be6c780569205b5
bapt2 <5711@holbertonstudents.com>
2022-10-13 05:50:13 -0700

    c more pointers

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,10 @@
+#include "main.c"
+/**
+ *reverse_array - function that reverse the content of an array of integer
+ *@a:integer
+ *@n:integer
+ *Return: 0
+ */
+void reverse_array(int *a, int n);
+{
+	
