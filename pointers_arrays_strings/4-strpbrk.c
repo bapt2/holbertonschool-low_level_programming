37e37738ec4bb64ee0da15cfe651ce6b4fe56ff7
bapt2 <5711@holbertonstudents.com>
2022-10-14 10:48:01 -0700

    even more pointers

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,8 @@
  *_strpbrk - function that searches a string for any of a set of bytes
  *@s: the string to be search
  *@accept: character
- *Return
+ *Return: pointer to the byte in s that matches one of the bytes in accept
+ *or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
