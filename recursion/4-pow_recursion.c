a0bfd3f135e37f8f51694c6ee58ee43f177284b6
bapt2 <5711@holbertonstudents.com>
2022-10-17 08:13:28 -0700

    c recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  *_pow_recursion - function that returns the value of x
- *@x
- *@y
+ *@x:int
+ *@y:int
  *Return: always 0
  */
 int _pow_recursion(int x, int y)
@@ -11,5 +11,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	if (x == 0)
 		return (0);
-	return x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
