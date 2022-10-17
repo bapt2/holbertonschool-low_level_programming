ef2820ff0a1926a7d6ce48e8a45443a7bd161901
bapt2 <5711@holbertonstudents.com>
2022-10-17 03:23:56 -0700

    c recursion

--- recursion.c/1-print_rev_recursion.c
+++ recursion.c/1-print_rev_recursion.c
@@ -1,19 +1,16 @@
 #include "main.h"
 /**
  *_print_rev_recursion - function that print a string in a reverse
- *
+ *@s: character
  *Return: Always 0
  */
 void _print_rev_recursion(char *s)
 {
-	int i;
-
 	if (*s)
 	{
-		_print_rev_recursion(s + 1, i);
-		_putchar(*s, i);
+		_print_rev_recursion(s + 1);
+		_putchar("%c", *s);
 	}
-	return (fp);
 }
 int main(void)
 {
