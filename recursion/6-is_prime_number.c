a2d860634305703cc4576ffad29add0255128a98
bapt2 <5711@holbertonstudents.com>
2022-10-18 02:22:08 -0700

    c recursion

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_prime - function that return if a number is prime
+ *@x:parameter
+ *@n:parameter
+ *Return: int
+ */
+int _prime(int x, int y)
+{
+	if (x <= 1 || n % y == 0)
+		return (0);
+	else if (n == y)
+		return (1);
+	else if (n > y)
+	{
+		_prime(n, y + 1)
+	}
+	return (1);
+/**
+ *is_prime_number- function that return 1 if the input int is prime number
+ *@n:int
+ *Return:1 if the input the int is a prime number otherwhise return 0
+ */
+
+int is_prime_number(int n)
+{
+	return (_prime(n, 2));
+
+}
