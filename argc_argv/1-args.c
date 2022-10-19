d5dde9eae280c48bb05ab8dba1c46ae994361e66
bapt2 <5711@holbertonstudents.com>
2022-10-19 07:12:45 -0700

    c - argc, argv

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-
+/**
+ *main - program that print the number of argument passed into it
+ *@argc: integer
+ *@argv: character
+ *Return: 0
+ */
 int main(int argc, char *argv[])
 {
-        int i = 0;
+	int i = 0;
 
-        printf("%d\n", argc);
-        while (i < argc - 1)
-        {
-                printf("%s",argv[i]);
-        }
-        return (0);
+	printf("%d\n", argc);
+	while (i < argc - 1)
+	{
+	printf("%s",argv[i]);
+	}
+	return (0);
 }
