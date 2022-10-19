5b2c707dccc5ca6faec654868ceea21ee7a3c41a
bapt2 <5711@holbertonstudents.com>
2022-10-19 07:07:39 -0700

    c - argc, argv

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
         int i = 0;
 
         printf("%d\n", argc);
-        while (i < argc - 1;)
+        while (i < argc - 1)
         {
                 printf("%s",argv[i]);
         }
