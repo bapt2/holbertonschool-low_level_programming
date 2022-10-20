d1cd1a2f5afb4b824bbb4b5769df32ed66288fea
bapt2 <5711@holbertonstudents.com>
2022-10-20 02:10:19 -0700

    c - argc, argv

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+/**
+ *main - program that multiplies two number
+ *@argc:integer
+ *@argv:character
+ *Return: 1 if the program does not receive two argument
+ */
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc != 3
-	for (i = 0; i < argc - 1; i++)
-	{
-		printf("%s",argv[i]);
-	}
-	return (0);
-}
+	if (argc != 3)
+		{
+			printf("Error\n");
+			exit(1);
+		}
+	    for (i = 0; i < argc - 1; i++)
+		    sum = sum + atoi(argv[i]);
+		printf("%d\n", sum);
+		return (0);
+		}
