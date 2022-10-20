1c971ad69fc7c78c7e94d1d13fe1ae761e88f5df
bapt2 <5711@holbertonstudents.com>
2022-10-20 07:05:49 -0700

    c - argc, argv

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,23 +9,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
-
-	if (argc != 3)
-	{
-		printf("0\n");
-		return (0);
-	}
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
+	{
+		if (!isdigit(argv[i][j]))
 		{
 			printf("Error\n");
 			return (1);
 		}
 	}
-		sum = sum + atoi(argv[i]);
+	sum += atoi(argv[i]);
+	}
 	printf("%d\n", sum);
 	return (0);
 }
