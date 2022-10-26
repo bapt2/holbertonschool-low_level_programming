f96e400133da06daa7e901ef920bb55f7cbc1ea2
bapt2 <5711@holbertonstudents.com>
2022-10-26 10:08:51 -0700

    more malloc,free

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -9,32 +9,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ar;
-	unsigned int i, j, k;
+	char *p;
+	unsigned int i = n, k;
 
 	if (s1 == NULL)
-		s1 = 0;
-	else
-	{
-		for (i = 0; s1[i]; i++)
-			;
-	}
+		s1 = "";
 	if (s2 == NULL)
-		s2 = 0;
-	else
+		s2 = "";
+
+	for (k = 0; s1[k]; k++)
+		i++;
+
+	p = malloc(sizeof(char) * (i + 1));
+
+	i = 0;
+
+	for (k = 0; s1[k]; k++)
 	{
-		for (j = 0; s2[j]; j++)
-			;
+		if (p == NULL)
+			return (NULL);
+		p[i++] = s1[k];
 	}
-	if (j > n)
-		j = n;
-	ar = malloc((i + j + 1) * sizeof(char));
-	if (ar == NULL)
-		return (NULL);
-	for (k = 0; k < i; k++)
-		ar[k] = s1[k];
-	for (k = 0; k < j; k++)
-		ar[k + i] = s2[k];
-	ar[i + j] = '\0';
-	return (ar);
+	for (k = 0; s2[k] && k < n; k++)
+	{
+		if (p == NULL)
+			return (NULL);
+		p[i++] = s2[k];
+	}
+	p[i] = '\0';
+
+	return (p);
 }
