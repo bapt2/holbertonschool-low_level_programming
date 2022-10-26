323adef3ec2bdeb8025d1ff9748a240efebbcc36
bapt2 <5711@holbertonstudents.com>
2022-10-26 00:31:42 -0700

    more malloc,free

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *malloc_checked - function that allocates memory using malloc
+ *@b: integer
+ *Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int  *p;
+
+	p = malloc(sizeof(b) * 2);
+	return (p);
+}
