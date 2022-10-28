8714f78196ed83cd77f4986c797d8cb241d5d391
bapt2 <5711@holbertonstudents.com>
2022-10-28 07:14:45 -0700

    more malloc,free

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -9,7 +9,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	ptr = malloc(old_size * new_size);
+	ptr = malloc(old_size * sizeof(new_size));
 	if (ptr == NULL)
 		return (NULL);
 
