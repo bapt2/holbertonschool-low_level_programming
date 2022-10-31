0a37fcd0c64d54c1a72ae0f8ed47f915fbdb857f
bapt2 <5711@holbertonstudents.com>
2022-10-31 02:29:56 -0700

    c structures, typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,16 @@
-#include <stdio.h>
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+#endif
