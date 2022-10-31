20547d4125b17352dc764916896215ccffc55c47
bapt2 <5711@holbertonstudents.com>
2022-10-31 04:09:52 -0700

    c structures, typedef

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,14 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (name != NULL)
+	if (d != NULL)
 	{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	}
-	if (name == NULL)
-	{
-	printf("ok\n");
 	}
 }
