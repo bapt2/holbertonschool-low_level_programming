3cd9f7192633fc9af001eb5746a10499a94102ea
bapt2 <5711@holbertonstudents.com>
2022-11-01 03:01:27 -0700

    c structures, typedef

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 /**
  *free_dog - function that frees dogs.
- *@p:
+ *@p: dog_t
  *Return: 0
  */
 void free_dog(dog_t *d)
 {
+	if (d != NULL)
+	{
 	free(d->owner);
 	free(d->name);
 	free(d);
+	}
 }
