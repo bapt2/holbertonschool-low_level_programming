fbc92ceca2b1291877a1be9b75a7f4b07f06e3a9
bapt2 <5711@holbertonstudents.com>
2022-11-01 09:17:54 -0700

    C - Function pointers

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -10,7 +10,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
+
 	if (array == NULL || size == '\0' || action == NULL)
 		return;
-	action(array[5]);
-}
+	for (i = 0; i < size; i++)
+		action(array[i]);
+		}
