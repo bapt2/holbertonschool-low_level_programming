b8d793313d8c5f5ee1db35dae38008f3d3515da7
bapt2 <5711@holbertonstudents.com>
2022-11-02 03:50:43 -0700

    C - Function pointers

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	if (array == NULL || size == '\0' || cmp == NULL)
 		return (-1);
-	if (array <= 0)
+	if (size <= 0)
 		return (-1);
 	return (size);
 }
