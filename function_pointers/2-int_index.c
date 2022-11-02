93ffc8398a6c8ac068cd9b760097d8d1fc75ae1a
bapt2 <5711@holbertonstudents.com>
2022-11-02 01:43:14 -0700

    C - Function pointers

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ *int_index: function that searches for an integer
+ *@array:array of the function
+ *@size:number of the elements in the array
+ *@cmp:pointer to the function
+ *Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array == NULL || size == '\0' || cmp == NULL)
+		return (0);
+}
