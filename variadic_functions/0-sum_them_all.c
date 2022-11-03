a4225b14db0f8a19ff26eda71c281dd5e86f3bdb
bapt2 <5711@holbertonstudents.com>
2022-11-03 10:33:37 -0700

    C - Variadic functions

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,6 +8,7 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int j = 0;
 	unsigned int i;
 	va_list args;
 
@@ -15,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-
+		j += va_arg(args, int);
 	va_end(args);
-	return (i);
+	return (j);
 }
