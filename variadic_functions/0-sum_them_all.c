c5b01d615e5e1563318d92b7fe34c76cd8bd3f51
bapt2 <5711@holbertonstudents.com>
2022-11-03 07:56:55 -0700

    C - Function pointers

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdlib.h>
+#include <stdarg.h>
 /**
  *sum_them_all - function that returns the sum of all its parameters.
  *@n: integer
@@ -7,8 +8,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	unsigned int j = 0;
+	va_list args;
+
+	va_start(args, n);
 	if (n == 0)
 		return (0);
+	for (i = 0; i < n; i++)
 
-	return (0);
+	va_end(args);
+	return (j);
 }
