a93df03babfa1187679a6009711df9c2f0fdd6bb
bapt2 <5711@holbertonstudents.com>
2022-11-03 06:47:05 -0700

    C - Function pointers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,15 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ *print_number - function that prints numbers, followed by a new line
+ *@separator:character
+ *@n: unsigned int
+ *Return: 0
+ */
+(const char *separator, const unsigned int n, ...)
+{
+	if (separator == NULL)
+		return (NULL);
+	return (0);
+}
