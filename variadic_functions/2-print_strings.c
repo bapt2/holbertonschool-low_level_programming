124c40c83dcae80a2ba584f4369483b8fe71eeac
bapt2 <5711@holbertonstudents.com>
2022-11-04 02:56:57 -0700

    C - Variadic functions

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *print_numbers - function that prints numbers, followed by a new line
+ *@separator:character
+ *@n: unsigned int
+ *Return: 0
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int j;
+	va_list args;
+
+	va_start(args, n);
+	for (j = 0; j < n; j++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && j != n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
