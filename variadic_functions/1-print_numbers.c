e4171f550f83cf08a190084de91c0985f38eb73d
bapt2 <5711@holbertonstudents.com>
2022-11-04 02:52:52 -0700

    C - Variadic functions

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,9 +15,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 		for (j = 0; j < n; j++)
 		{
-			if (separator != NULL)
+			printf("%d", va_arg(args, int));
+			if (separator != NULL && j != n - 1)
 			{
-				printf("%d", va_arg(args, int));
 				printf("%s", separator);
 			}
 		}
