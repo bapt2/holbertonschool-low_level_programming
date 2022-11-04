e234c0a8e993db0c1544597c5c5714a3b850527e
bapt2 <5711@holbertonstudents.com>
2022-11-04 01:39:40 -0700

    C - Variadic functions

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,23 +2,21 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- *print_number - function that prints numbers, followed by a new line
+ *print_numbers - function that prints numbers, followed by a new line
  *@separator:character
  *@n: unsigned int
  *Return: 0
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int j = 0;
-	unsigned int i;
+	unsigned int j;
 	va_list args;
 
 	va_start(args, n);
-		for (i = 0; i < n; i++)
-			printf("%d ;\n", n);
+		for (j = 0; j < n; j++)
+			printf("%d; ", va_arg(args, int));
 		if (separator == NULL)
-		return;
-		j += va_arg(args, int);
+			printf("\n");
 		va_end(args);
 	return;
 }
