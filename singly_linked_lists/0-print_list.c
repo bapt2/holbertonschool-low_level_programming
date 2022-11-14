5def743d995162a3cc18963aa759bb3b88223786
bapt2 <5711@holbertonstudents.com>
2022-11-14 08:37:45 -0800

    update

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,14 +10,12 @@ size_t print_list(const list_t *h)
 	int i;
 
 	for (i = 0; h != NULL; i++)
-	if (!h->str)
 	{
-		printf("[0] (nil)");
+		if (!h->str)
+		printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 	}
-	else
-	{
-		printf("[%d]%s\n", h->len, h->str);
-	}
-	h = h->next;
 	return (i);
 }
