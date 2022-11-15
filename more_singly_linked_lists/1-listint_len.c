a5bbed5255f2dc9c9eccda7ac3a0f0414b35d01d
bapt2 <5711@holbertonstudents.com>
2022-11-15 07:11:19 -0800

    added files

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list
+ * @h: pointer on struct
+ * Return: 0
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (!h->n)
+			printf("[0] (nil)\n");
+		h = h->next;
+	}
+	return (i);
+}
