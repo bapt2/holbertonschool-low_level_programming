7e02a26578b2de37f9cdd252d6be182d2314126b
bapt2 <5711@holbertonstudents.com>
2022-11-15 07:02:06 -0800

    added files

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a list_t list.
+ * @h: str
+ * Return: the number of nodes and [0] (nil) if str is NULL
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (!h->n)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] \n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
