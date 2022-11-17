fb913ed570207f0adc3334d7aa40e8181e3d42fd
bapt2 <5711@holbertonstudents.com>
2022-11-17 11:08:00 -0800

    test2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -8,8 +8,10 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (!index)
+	int x;
+
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	n = (n >> index) & 1;
-	return (index);
+	x = (n >> index) & 1;
+	return (x);
 }
