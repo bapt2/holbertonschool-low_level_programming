862605de5afd110b6a4a582402920af879c6f465
bapt2 <5711@holbertonstudents.com>
2022-11-17 06:41:58 -0800

    added file

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - function that deletes a specifique node of a list
+ * @head: head o the list
+ * @index
+ * Return: 0 if the linked list is empty else the head node's data
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *del, *tempn;
+
+	del = malloc(sizeof(listint_t));
+	if (!del)
+	{
+		return (-1);
+	}
+	tempn = *head;
+	if (index == 0)
+	{
+		del->next = tempn;
+		*head = del;
+		return (-1);
+	}
+	tempn = *head;
+	for (i = 0; i < (index - 1); i++)
+	{
+		if (!tempn || !tempn->next)
+			return (0);
+		tempn = tempn->next;
+	}
+	del->next = tempn->next;
+	tempn->next = del;
+
+	return (1);
+}
