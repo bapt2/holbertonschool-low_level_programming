3d35c2d323e75a93254b61d5c885985ca52b4a0a
bapt2 <5711@holbertonstudents.com>
2022-11-17 05:34:22 -0800

    test 5

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -25,11 +25,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = newn;
 		return (*head);
 	}
-	else
+	tempn = *head;
+	for (i = 0; i < (idx - 1); i++)
 	{
-		tempn = *head;
-		for (i = 0; i < (idx - 1); i++)
-			tempn = tempn->next;
+		if (!tempn || !tempn->next)
+			return (NULL);
+		tempn = tempn->next;
 	}
 	newn->next = tempn->next;
 	tempn->next = newn;
