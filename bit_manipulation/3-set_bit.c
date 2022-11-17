eb3973082352bd70406d1ad5cda8ecd7179b6c96
bapt2 <5711@holbertonstudents.com>
2022-11-17 11:43:06 -0800

    fixed kernel style

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -8,7 +8,7 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(size_t) * 8 ))
+	if (index >= (sizeof(size_t) * 8))
 		return (-1);
 	*n ^= (1 << index);
 	return (1);
