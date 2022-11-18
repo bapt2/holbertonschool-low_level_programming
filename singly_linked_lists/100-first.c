58a37e86aed70bc4bfbaf54df46d0960226442bf
bapt2 <5711@holbertonstudents.com>
2022-11-18 01:30:14 -0800

    fixed betty

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * beforthefunction - function that print before the main function is executed
+ * beforethefunction - function that print before the main function is executed
  * Return: result
 */
-void beforethefunction (void) __attribute__ ((constructor));
-void beforethefunction (void)
+void beforethefunction(void) __attribute__ ((constructor));
+void beforethefunction(void)
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
