adc770e56b2c3e12f81688ee02c4d98cd8fb3981
bapt2 <5711@holbertonstudents.com>
2022-11-18 00:18:17 -0800

    fixed kernel style

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -3,14 +3,16 @@
 /**
  * flip_bits -function that returns the number of bits
  * you would need to flip to get from one number to another.
- * @n:
- * @m:
- * Return:
+ * @n: n
+ * @m: m
+ * Return: count
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	size_t count = 0, o;
-	for (o = n ^ m; o != 0; o = o >> 1)
-		count += o & 1;;
+	size_t count = 0;
+	size_t o = n ^ m;
+
+	for (; o != 0; o = o >> 1)
+		count += o & 1;
 	return (count);
 }
