0c0e477dfe77e22504a4692defbd0b3f87f509cd
bapt2 <5711@holbertonstudents.com>
2022-11-21 07:56:54 -0800

    test 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,8 +8,19 @@
 */
 int create_file(const char *filename, char *text_content)
 {
+	int o, i;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
-	return (-1);
+	o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (o == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		write(o, text_content, i);
+	}
+	close(o);
+	return (1);
 }
