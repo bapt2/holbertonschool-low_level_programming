adb100edc680125b476cc4eb38c0520aaf2c1b33
bapt2 <5711@holbertonstudents.com>
2022-11-21 06:57:30 -0800

    added file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * create_file - function that create a file
+ * @filename: the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: -1 on failur or 1 on success
+*/
+int create_file(const char *filename, char *text_content)
+{
+	if (!filename || !text_content)
+		return (-1);
+	return (1);
+}
