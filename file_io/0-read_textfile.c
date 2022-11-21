b236dfd77022566af97934ed4f15687044f58fb0
bapt2 <5711@holbertonstudents.com>
2022-11-21 02:37:02 -0800

    added files

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints it
+ * @filename: name of the file
+ * @letters: the number of letters it should read and print
+ * Return: 0 if filename is NULL, the file can't be open and if write failed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (!filename)
+		return (0);
+	if (!letters)
+		return (0);
+	return (0);
+}
