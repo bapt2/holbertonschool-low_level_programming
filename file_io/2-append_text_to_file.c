3d494cbf7f470b21b8611bf8d74ef145d87f94d1
bapt2 <5711@holbertonstudents.com>
2022-11-22 02:11:46 -0800

    test 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,21 +8,19 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, i;
+	int o, i, w;
 
 	if (!filename)
 		return (-1);
-	o = open(filename, O_RDWR, 0664);
-	if (o == -1)
-		return (-1);
-	if (!text_content)
-		return (1);
+	o = open(filename, O_WRONLY | O_APPEND);
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
-		write(o, text_content, i);
+		w = write(o, text_content, i);
 	}
+	if (w == -1 || o == -1)
+		return (-1);
 	close(o);
 	return (1);
 }
