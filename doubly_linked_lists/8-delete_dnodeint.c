cb173e22a1b8a31465c13d27e2c65302b70f8036
bapt2 <5711@holbertonstudents.com>
2022-11-23 01:40:47 -0800

    test 1

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,27 +9,29 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i;
-	dlistint_t *del, *tmp;
+	dlistint_t *del, *tempn;
 
 	del = *head;
 	if (!del)
+	{
 		return (-1);
-	tmp = *head;
+	}
+	tempn = *head;
 	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(del);
 		return (1);
 	}
-	tmp = *head;
+	tempn = *head;
 	for (i = 0; i < (index - 1); i++)
 	{
 		if (!del->next)
 			return (-1);
 		del = del->next;
 	}
-	tmp = del->next;
-	del->next = tmp->next;
-	free(tmp);
+	tempn = del->next;
+	del->next = tempn->next;
+	free(tempn);
 	return (1);
 }
