1230a8e36d43c78cc85a97be5e3c2f49359eb525
bapt2 <5711@holbertonstudents.com>
2022-11-23 06:15:11 -0800

    added files

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: the size of the array
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	if (!size)
+		return (NULL);
+	return (0);
+}
