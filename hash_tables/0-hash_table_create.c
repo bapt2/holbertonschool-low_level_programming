6a14a40bfb2fd2446a1a3652253c7b5c13b6e273
bapt2 <5711@holbertonstudents.com>
2022-11-24 03:01:15 -0800

    test 1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,7 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	size_t i;
+	hash_table_t *hash;
+
 	if (!size)
 		return (NULL);
-	return (0);
+	hash = malloc(sizeof(hash_table_t));
+	if (!hash)
+		return (0);
+	hash->size = size;
+	hash->array = malloc(sizeof(hash_table_t *) * size);
+	if (!hash->array)
+		return (0);
+	for (i = 0; i < size; i++)
+		hash->array[i] = NULL;
+	return (hash);
 }
